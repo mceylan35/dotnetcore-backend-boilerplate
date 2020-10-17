c84548f70844e38aa14e93275cd96d423677f5a7
mceylan35 <mehmetceylan2696@gmail.com>
2020-10-18 01:54:43 +0300

    Interception alt yapısı eklendi

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿
+using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple=true,Inherited =true)]
+    public class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+        public virtual void Intercept(IInvocation invocation)
+        {
+          
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptors.cs
@@ -0,0 +1,55 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterceptors:MethodInterceptionBaseAttribute
+    {
+        //Method çalışmadan önce çalışır, Invocation çalıştırılmaya çalışan method
+        protected virtual void OnBefore(IInvocation ınvocation)
+        {
+
+        }
+        protected virtual void OnAfter(IInvocation ınvocation)
+        {
+
+        }
+        protected virtual void OnException(IInvocation ınvocation)
+        {
+
+        }
+        protected virtual void OnSuccess(IInvocation ınvocation)
+        {
+
+        }
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception)
+            {
+
+                isSuccess = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+                else
+                {
+                    OnAfter(invocation);
+                }
+            }
+        }
+    }
+}
